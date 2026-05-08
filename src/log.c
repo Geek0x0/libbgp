8bf03af836aa6c4726949e6626d393d7056b1a67
Kody <kaidi.shi.1121@gmail.com>
2026-05-08 12:23:58 -0700

    fix: synchronize default logger access

--- src/log.c
+++ src/log.c
@@ -1,12 +1,31 @@
 #include <stdarg.h>
 
 #include "libbgp/log.h"
+#include "internal.h"
 
 static libbgp_logger_t g_default_logger = {
     NULL,
     NULL,
     LIBBGP_LOG_INFO
 };
+
+#ifdef BGP_THREADSAFE
+static bgp_lock_t g_default_logger_lock = PTHREAD_MUTEX_INITIALIZER;
+#endif
+
+static void default_logger_lock(void)
+{
+#ifdef BGP_THREADSAFE
+    bgp_lock(&g_default_logger_lock);
+#endif
+}
+
+static void default_logger_unlock(void)
+{
+#ifdef BGP_THREADSAFE
+    bgp_unlock(&g_default_logger_lock);
+#endif
+}
 
 void libbgp_logger_init(libbgp_logger_t *logger)
 {
@@ -21,12 +40,15 @@ void libbgp_logger_init(libbgp_logger_t *logger)
 
 void libbgp_set_default_logger(const libbgp_logger_t *logger)
 {
+    default_logger_lock();
+
     if (logger) {
         g_default_logger = *logger;
-        return;
+    } else {
+        libbgp_logger_init(&g_default_logger);
     }
 
-    libbgp_logger_init(&g_default_logger);
+    default_logger_unlock();
 }
 
 libbgp_logger_t *libbgp_get_default_logger(void)
@@ -37,7 +59,15 @@ libbgp_logger_t *libbgp_get_default_logger(void)
 void libbgp_log(libbgp_logger_t *logger, libbgp_log_level_t level, const char *fmt, ...)
 {
     va_list args;
-    libbgp_logger_t *target = logger ? logger : libbgp_get_default_logger();
+    libbgp_logger_t default_logger;
+    libbgp_logger_t *target = logger;
+
+    if (!target) {
+        default_logger_lock();
+        default_logger = g_default_logger;
+        default_logger_unlock();
+        target = &default_logger;
+    }
 
     if (!target->log_fn || level > target->level || !fmt) {
         return;
